22128c1083aa25bc99e42720494fc9cb82d05990
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2023-11-17 22:59:40 -0600

    finished project

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,10 +4,7 @@ import satData from "./satData";
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div className="flex-container">
-      {/* Button to display all satellites */}
       <button onClick={() => setSat(satData)}>All Orbits</button>
-
-      {/* Buttons to filter by satellite type */}
       {displaySats.map((sat, id) => (
         <div key={id}>
           <button onClick={() => filterByType(sat)}>
