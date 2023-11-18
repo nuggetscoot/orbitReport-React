8a891aedcd397e30f9e29069309ea21352beff77
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2023-11-17 22:59:02 -0600

    finished project

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,21 @@
+import React from "react";
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {/* Button to display all satellites */}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+
+      {/* Buttons to filter by satellite type */}
+      {displaySats.map((sat, id) => (
+        <div key={id}>
+          <button onClick={() => filterByType(sat)}>
+            {sat} Orbit
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
-
 export default Buttons;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,36 @@
+import React from "react";
+
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((data, id) => (
+          <tr key={id}>
+            <td>{data.name}</td>
+
+            <td>{data.type}</td>
+
+            <td>{data.launchDate}</td>
+
+            <td>
+              {data.operational ? (
+                <span style={{ color: "green" }}>Active</span>
+              ) : (
+                <span style={{ color: "red" }}>Inactive</span>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
